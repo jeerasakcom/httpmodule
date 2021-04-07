323b855ffd591a22f260226e859cd7026b9aed5d
jeerasakcom <67991019+jeerasakcom@users.noreply.github.com>
2021-04-07 16:06:10 +0700

    saaaa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'httpmodule';
+
+  //http get
+  passdataArray: any[] = [];
+  constructor(private http: HttpClient) {
+    http.get<any[]>('https://jsonplaceholder.typicode.com/posts').subscribe(result => {
+      this.passdataArray = result;
+
+      console.log(JSON.stringify(this.passdataArray));
+    });
+  }
+
+
 }
